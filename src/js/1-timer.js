f1565a0018a6864b35ccc77a65847ac304a53f24
daria <wedory27@gmail.com>
2025-02-22 15:43:33 +0200

    timer firts adding

---
+++ src/js/1-timer.js
@@ -0,0 +1,31 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const dateInput = document.querySelector("#datetime-picker");
+const startButton = document.querySelector("[data-start]");
+
+let userSelectedDate = null;
+startButton.disabled = true;
+
+flatpickr(dateInput, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate <= new Date()) {
+      iziToast.error({
+        title: "Error",
+        message: "Please choose a date in the future",
+      });
+      startButton.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      startButton.disabled = false;
+    }
+  },
+});
